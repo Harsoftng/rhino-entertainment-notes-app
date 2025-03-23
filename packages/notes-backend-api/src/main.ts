dba68efe120ae84389635ee2b9807aee9eafc57d
Ibrahim Haruna <harsoftng@gmail.com>
2025-03-23 14:34:52 +0100

    feature: 1. setup global interceptor 2. setup global validation pipe 3. setup helmet for extra security

--- packages/notes-backend-api/src/main.ts
+++ packages/notes-backend-api/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from "process";
+import helmet from 'helmet';
+import {ValidationPipe} from "@nestjs/common";
+import {TransformInterceptor} from "./shared/transform.interceptor";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8080);
+  const port = Number(process.env.BACKEND_SERVICE_PORT || 8080);
+
+  const app = await NestFactory.create(AppModule, {cors: true});
+
+  app.use(helmet());
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  await app.listen(port, () => {
+    console.log(`Notes backend started on port ${port}`);
+  });
 }
 bootstrap();
