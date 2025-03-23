b1e17adf2c9b18347b58a9c264089ef72a55fc9c
Ibrahim Haruna <harsoftng@gmail.com>
2025-03-23 14:44:40 +0100

    feature: 1. setup MikroOrm app-wide config file for establishing connections to postgres database 2. setup database module 3. setup redis service to interact with the redis cache store

---
+++ packages/notes-backend-api/src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { RedisService } from './redis.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
+@Module({
+  imports: [
+    ConfigModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        console.log({ redisUrl: configService.get<string>('REDIS_URL') });
+        return {
+          stores: [createKeyv(configService.get<string>('REDIS_URL'))],
+        };
+      },
+      inject: [ConfigService],
+    }),
+    MikroOrmModule.forRoot(),
+    MikroOrmModule.forFeature({}),
+  ],
+  providers: [RedisService],
+  exports: [RedisService],
+})
+export class DatabaseModule {}
